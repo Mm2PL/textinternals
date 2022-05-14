271f40dce37aa6d46f14e54bd5cd1c283219e67b
Mm2PL <miau@mail.kotmisia.pl>
2022-05-15 00:46:51 +0200

    Pull and autocommit

--- sync.sh
+++ sync.sh
@@ -5,4 +5,5 @@ TAGNAME=$(git -C text tag | tail -n 1)
 git -C text checkout "$TAGNAME"  # check out latest tag
 cp -vr text/internal/* ./
 git add .
+git commit -m "Update to $TAGNAME"
 git tag "$TAGNAME"
